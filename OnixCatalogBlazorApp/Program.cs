8b315f3fbdbd059f4e90458f1f9a0245c1bed4d2
jaerith <Aaron.Kendall@gmail.com>
2023-01-09 18:10:21 -0500

    Added necessary configuration lines.

--- OnixCatalogBlazorApp/Program.cs
+++ OnixCatalogBlazorApp/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using OnixCatalogBlazorApp;
+using OnixCatalogBlazorApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddTransient<IFileService, FileService>();
+builder.Services.AddScoped<ICatalogService, CatalogService>();
 
 await builder.Build().RunAsync();
