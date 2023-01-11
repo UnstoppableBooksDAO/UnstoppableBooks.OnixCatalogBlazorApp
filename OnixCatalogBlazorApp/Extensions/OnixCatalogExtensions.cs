d7af9fa64bf4d079d81d13c6834d1e35d61e17a5
jaerith <Aaron.Kendall@gmail.com>
2023-01-11 18:32:34 -0500

    Initial revision.

---
+++ OnixCatalogBlazorApp/Extensions/OnixCatalogExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+using OnixCatalogBlazorApp.Models;
+
+namespace OnixCatalogBlazorApp.Extensions
+{
+    public static class OnixCatalogExtensions
+    {
+        public static string Serialize(this BookItem bookItem)
+        {
+            return JsonConvert.SerializeObject(bookItem);
+        }
+    }
+}
