05a1344ada0a887aed0011583b1f3b6d92a63c89
jaerith <Aaron.Kendall@gmail.com>
2023-01-16 10:36:31 -0500

    Added usage of browser storage when saving the catalog data.

--- OnixCatalogBlazorApp/Program.cs
+++ OnixCatalogBlazorApp/Program.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using OnixCatalogBlazorApp;
+using OnixCatalogBlazorApp.Proxy;
 using OnixCatalogBlazorApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<CacheStorageProxy>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
