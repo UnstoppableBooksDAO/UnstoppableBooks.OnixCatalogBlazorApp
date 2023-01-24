f46c852e418124e083f2d556de8a812d3330a9b5
jaerith <Aaron.Kendall@gmail.com>
2023-01-23 19:02:43 -0500

    Added annotations to BookItem class.

--- OnixCatalogBlazorApp/Models/BookItem.cs
+++ OnixCatalogBlazorApp/Models/BookItem.cs
@@ -1,15 +1,45 @@
-﻿namespace OnixCatalogBlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+/*
+public class Starship
+{
+    [Required]
+    [StringLength(16, ErrorMessage = "Identifier too long (16 character limit).")]
+    public string? Identifier { get; set; }
+
+    public string? Description { get; set; }
+
+    [Required]
+    public string? Classification { get; set; }
+
+    [Range(1, 100000, ErrorMessage = "Accommodation invalid (1-100000).")]
+    public int MaximumAccommodation { get; set; }
+
+    [Required]
+    [Range(typeof(bool), "true", "true",
+        ErrorMessage = "This form disallows unapproved ships.")]
+    public bool IsValidatedDesign { get; set; }
+
+    [Required]
+    public DateTime ProductionDate { get; set; }
+}
+*/
+
+namespace OnixCatalogBlazorApp.Models
 {
     public class BookItem
     {
         public long Ean { get; set; }
 
+        [Required]
+        [StringLength(512, ErrorMessage = "Title too long (512 character limit).")]
         public string? Title { get; set; }
 
         public string? AuthorName { get; set; }
 
         public string? AuthorEthereumId { get; set; }
 
+        [Range(1, 100000, ErrorMessage = "Accommodation invalid (1-100000).")]
         public decimal? Price { get; set; }
 
         public string? PrimaryBISAC { get; set; }
