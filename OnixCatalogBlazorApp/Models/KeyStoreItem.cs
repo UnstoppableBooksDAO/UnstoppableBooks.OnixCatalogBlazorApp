df80abd3529ebabe87113eba885b5fa557dfdf56
jaerith <Aaron.Kendall@gmail.com>
2023-02-01 23:02:21 -0500

    Initial implementation of KeyStore page.

---
+++ OnixCatalogBlazorApp/Models/KeyStoreItem.cs
@@ -0,0 +1,14 @@
+﻿namespace OnixCatalogBlazorApp.Models
+{
+    public class KeyStoreItem
+    {
+        public string? PrivateKey { get; set; }
+
+        public string? Password { get; set; }
+
+        public KeyStoreItem() 
+        {
+            PrivateKey = Password = null;
+        }
+    }
+}
