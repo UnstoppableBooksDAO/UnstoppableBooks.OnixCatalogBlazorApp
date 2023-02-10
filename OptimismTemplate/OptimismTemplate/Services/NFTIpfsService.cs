bdeb02fd2bdff68ff9df9222d5442ef199fffa2c
jaerith <Aaron.Kendall@gmail.com>
2023-02-10 12:09:19 -0500

    Added service class for IPFS and NFTs, copied from Juan Blanco's test project.

---
+++ OptimismTemplate/OptimismTemplate/Services/NFTIpfsService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.Contracts.Standards.ERC721;
+using Nethereum.Web3;
+using Newtonsoft.Json;
+
+namespace OptimismTemplate.Services
+{
+    public class NFTIpfsService
+    {
+        private readonly string _userName;
+        private readonly string _password;
+        private readonly string _ipfsUrl;
+
+        public NFTIpfsService(string ipfsUrl)
+        {
+            _ipfsUrl = ipfsUrl;
+        }
+
+        public NFTIpfsService(string ipfsUrl, string userName, string password) : this(ipfsUrl)
+        {
+            _userName = userName;
+            _password = password;
+        }
+
+        public Task<IPFSFileInfo> AddNftsMetadataToIpfsAsync<T>(T metadata, string fileName) where T : NftMetadata
+        {
+            var ipfsClient = GetSimpleHttpIpfs();
+            return ipfsClient.AddObjectAsJson(metadata, fileName);
+        }
+
+        public async Task<IPFSFileInfo> AddFileToIpfsAsync(string path)
+        {
+            var file = new FileInfo(path);
+            var ipfsClient = GetSimpleHttpIpfs();
+            var node = await ipfsClient.AddAsync(File.ReadAllBytes(path), file.Name);
+            return node;
+        }
+
+        private IpfsHttpService GetSimpleHttpIpfs()
+        {
+            if (_userName == null) return new IpfsHttpService(_ipfsUrl);
+            return new IpfsHttpService(_ipfsUrl, _userName, _password);
+        }
+    }
+}
