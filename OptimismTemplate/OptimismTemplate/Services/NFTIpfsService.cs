b9d262ce3bd9fa8cfa2d965a1c820004c261cab4
jaerith <Aaron.Kendall@gmail.com>
2023-02-20 15:21:39 -0500

    Added Get() methods for retrieving data from IPFS.

--- OptimismTemplate/OptimismTemplate/Services/NFTIpfsService.cs
+++ OptimismTemplate/OptimismTemplate/Services/NFTIpfsService.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
+using Ipfs;
+using Ipfs.Http;
 using Nethereum.Contracts.Standards.ERC721;
 using Nethereum.Web3;
 using Newtonsoft.Json;
@@ -46,6 +51,37 @@ namespace OptimismTemplate.Services
             var ipfsClient = GetSimpleHttpIpfs();
             var node = await ipfsClient.AddAsync(File.ReadAllBytes(path), file.Name);
             return node;
+        }
+
+        static public async Task<string> GetStringFromIpfsGateway(string relativePath, bool addIpfsSuffix = true, string ipfsGateway = "https://gateway.ipfs.io/")
+        {
+            var uri = new Uri(ipfsGateway);
+
+            if (addIpfsSuffix) uri = new Uri(uri, "ipfs");
+
+            var fullUriPath = uri.AbsoluteUri + Path.DirectorySeparatorChar + relativePath;
+
+            var fullUri = new Uri(fullUriPath);
+            using (var client = new HttpClient())
+            {
+                return await client.GetStringAsync(fullUri).ConfigureAwait(false);
+            }
+        }
+
+        static public async Task<T> GetJsonObjectFromIpfsGateway<T>(string relativePath, bool addIpfsSuffix = true, string ipfsGateway = "https://gateway.ipfs.io/")
+        {            
+            var uri = new Uri(ipfsGateway);
+
+            if (addIpfsSuffix) uri = new Uri(uri, "ipfs");
+
+            var fullUriPath = uri.AbsoluteUri + Path.DirectorySeparatorChar + relativePath;
+
+            var fullUri = new Uri(fullUriPath);
+            using (var client = new HttpClient())
+            {
+                var json = await client.GetStringAsync(fullUri).ConfigureAwait(false);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
         }
 
         private IpfsHttpService GetSimpleHttpIpfs()
